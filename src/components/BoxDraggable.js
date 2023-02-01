e5703a3e8181cd15e9cd6e45c0a2cefa5412c828
Jesusin <jpindado@consentio.com>
2023-02-01 21:50:07 +0100

    quick soluction from move all selected boxes

--- src/components/BoxDraggable.js
+++ src/components/BoxDraggable.js
@@ -2,34 +2,59 @@ import interact from 'interactjs';
 import { observer } from 'mobx-react';
 import React, { useCallback, useEffect, useRef } from 'react';
 
+import store from '../stores/MainStore';
+
 function BoxDraggable({ id, color, width, height, left, top, children, box }) {
   const boxEl = useRef(null);
 
-  const getPosition = useCallback(
-    event => {
-      // keep the dragged position in the data-x/data-y attributes
-      const x = (Number(boxEl.current.getAttribute('data-x')) || 0) + event.dx;
-      const y = (Number(boxEl.current.getAttribute('data-y')) || 0) + event.dy;
-      return { x, y };
-    },
-    [boxEl]
-  );
+  const getPosition = useCallback((event, el) => {
+    // keep the dragged position in the data-x/data-y attributes
+    const x = (Number(el.getAttribute('data-x')) || 0) + event.dx;
+    const y = (Number(el.getAttribute('data-y')) || 0) + event.dy;
+    return { x, y };
+  }, []);
 
   useEffect(() => {
     interact(boxEl.current).draggable({
       autoScroll: true,
       listeners: {
         move: event => {
-          const { x, y } = getPosition(event);
+          const { x, y } = getPosition(event, boxEl.current);
           // translate the element
           boxEl.current.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
           // update the posiion attributes
           boxEl.current.setAttribute('data-x', x);
           boxEl.current.setAttribute('data-y', y);
+          if (box.selected) {
+            document
+              .querySelectorAll('[data-selected="true"]')
+              .forEach(selectedEl => {
+                if (selectedEl !== boxEl.current) {
+                  const elPos = getPosition(event, selectedEl);
+                  selectedEl.style.transform =
+                    'translate(' + elPos.x + 'px, ' + elPos.y + 'px)';
+                  // update the posiion attributes
+                  selectedEl.setAttribute('data-x', elPos.x);
+                  selectedEl.setAttribute('data-y', elPos.y);
+                }
+              });
+          }
         },
         end(event) {
-          const { x, y } = getPosition(event);
+          const { x, y } = getPosition(event, boxEl.current);
           box.move(y, x);
+          if (box.selected) {
+            document
+              .querySelectorAll('[data-selected="true"]')
+              .forEach(selectedEl => {
+                if (selectedEl !== boxEl.current) {
+                  const elPos = getPosition(event, selectedEl);
+                  store.boxes
+                    .find(b => b.id === selectedEl.id)
+                    .move(elPos.y, elPos.x);
+                }
+              });
+          }
         },
       },
     });
@@ -42,6 +67,7 @@ function BoxDraggable({ id, color, width, height, left, top, children, box }) {
       id={id}
       className='box'
       ref={boxEl}
+      data-selected={box.selected}
       data-x={left}
       data-y={top}
       style={{
